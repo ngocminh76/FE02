e6037dadcdf7f0d5bc566067f27ac78ffab1d54f
ngocminh76 <83106142+ngocminh76@users.noreply.github.com>
2025-06-28 06:06:18 +0700

    BAI 18

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,9 +1,12 @@
+import { Outlet } from "react-router-dom"
+import AppHeader from "./components/layout/app.header"
 
 function Layout() {
 
   return (
     <div>
-      Hello world Nguyễn Ngọc Minh Bài 17
+      <AppHeader />
+      <Outlet />
     </div>
   )
 }
