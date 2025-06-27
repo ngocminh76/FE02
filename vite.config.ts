da2854d4ca9afb86b8b8227fb7b7c56457fa13be
ngocminh76 <83106142+ngocminh76@users.noreply.github.com>
2025-06-28 06:38:08 +0700

    Tao duong dan tuyet doi, video 19

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import dns from 'dns'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
+
+
+dns.setDefaultResultOrder('verbatim')
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   server: {
     port: 3000, // 👈 override mặc định
   },
+
+  // css: {
+  //   preprocessorOptions: {
+  //     scss: {
+  //       api: 'modern-compiler' // or "modern"
+  //     }
+  //   }
+  // }
 })
