86d0fbc878e1692a413da8a439dc4270dfb15a79
ngocminh76 <83106142+ngocminh76@users.noreply.github.com>
2025-07-12 16:12:51 +0700

    Video 45

--- src/components/admin/user/table.user.tsx
+++ src/components/admin/user/table.user.tsx
@@ -1,9 +1,9 @@
 import { getUsersAPI } from '@/services/api';
-import { PlusOutlined } from '@ant-design/icons';
+import { DeleteTwoTone, EditTwoTone, PlusOutlined } from '@ant-design/icons';
 import type { ActionType, ProColumns } from '@ant-design/pro-components';
 import { ProTable, TableDropdown } from '@ant-design/pro-components';
 import { Button, Space, Tag } from 'antd';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 
 const columns: ProColumns<IUserTable>[] = [
     {
@@ -14,6 +14,12 @@ const columns: ProColumns<IUserTable>[] = [
     {
         title: '_id',
         dataIndex: '_id',
+        hideInSearch: true,
+        render(dom, entity, index, action, schema) {
+            return (
+                <a href='#'>{entity._id}</a>
+            )
+        }
     },
     {
         title: 'Full Name',
@@ -22,6 +28,7 @@ const columns: ProColumns<IUserTable>[] = [
     {
         title: 'Email',
         dataIndex: 'email',
+        copyable: true
     },
     {
         title: 'Created At',
@@ -29,13 +36,29 @@ const columns: ProColumns<IUserTable>[] = [
     }
     ,
     {
-        title: 'Updated At',
-        dataIndex: 'updatedAt',
+        title: 'Action',
+        hideInSearch: true,
+        render(dom, entity, index, action, schema) {
+            return (
+                <>
+                    <EditTwoTone twoToneColor="#f57800" style={{ cursor: "pointer", marginRight: 15 }} />
+                    <DeleteTwoTone twoToneColor="#ff4d4f" style={{ cursor: 'pointer' }} />
+                </>
+            )
+        },
     }
 ];
 
 const TableUser = () => {
     const actionRef = useRef<ActionType>();
+
+    const [meta, setMeta] = useState({
+        current: 1,
+        pageSize: 5,
+        pages: 0,
+        total: 0
+    })
+
     return (
         <>
             <ProTable<IUserTable>
@@ -45,22 +68,35 @@ const TableUser = () => {
                 request={async (params, sort, filter) => {
                     console.log(sort, filter);
                     const res = await getUsersAPI();
-                    // const data = await (await fetch('https://proapi.azurewebsites.net/github/issues')).json()
+                    if (res.data) {
+                        setMeta(res.data.meta)
+                    }
                     return {
                         // data: data.data,
                         data: res.data?.result,
-                        "page": 1,
-                        "success": true,
-                        "total": res.data?.meta.total
-                        // "total": 30
+                        page: 1,
+                        success: true,
+                        total: res.data?.meta.total
                     }
-
                 }}
                 editable={{
                     type: 'multiple',
                 }}
 
-                rowKey="id"
+                rowKey="_id"
+                pagination={
+                    {
+                        current: meta.current,
+                        pageSize: meta.pageSize,
+                        showSizeChanger: true,
+                        total: meta.total,
+                        showTotal: (total, range) => {
+                            return (
+                                <div>{range[0]}-{range[1]} trên {total} rows</div>
+                            )
+                        }
+                    }
+                }
                 search={{
                     labelWidth: 'auto',
                 }}
