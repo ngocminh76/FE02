23ce5fc6c72d056165b1a564515b796fbc74dcb5
ngocminh76 <83106142+ngocminh76@users.noreply.github.com>
2025-07-13 15:17:23 +0700

    Video 49

--- src/components/admin/user/table.user.tsx
+++ src/components/admin/user/table.user.tsx
@@ -5,61 +5,9 @@ import type { ActionType, ProColumns } from '@ant-design/pro-components';
 import { ProTable } from '@ant-design/pro-components';
 import { Button } from 'antd';
 import { useRef, useState } from 'react';
+import DetailUser from './detail.user';
 
-const columns: ProColumns<IUserTable>[] = [
-    {
-        dataIndex: 'index',
-        valueType: 'indexBorder',
-        width: 48,
-    },
-    {
-        title: 'Id',
-        dataIndex: '_id',
-        hideInSearch: true,
-        render(dom, entity, index, action, schema) {
-            return (
-                <a href='#'>{entity._id}</a>
-            )
-        }
-    },
-    {
-        title: 'Full Name',
-        dataIndex: 'fullName',
-    },
-    {
-        title: 'Email',
-        dataIndex: 'email',
-        copyable: true
-    },
-    {
-        title: 'Created At',
-        dataIndex: 'createdAt',
-        valueType: 'date',
-        sorter: true,
-        hideInSearch: true
-    }
-    ,
-    {
-        title: 'Created At',
-        dataIndex: 'createdAtRange',
-        valueType: 'dateRange',
-        sorter: true,
-        hideInTable: true
-    }
-    ,
-    {
-        title: 'Action',
-        hideInSearch: true,
-        render(dom, entity, index, action, schema) {
-            return (
-                <>
-                    <EditTwoTone twoToneColor="#f57800" style={{ cursor: "pointer", marginRight: 15 }} />
-                    <DeleteTwoTone twoToneColor="#ff4d4f" style={{ cursor: 'pointer' }} />
-                </>
-            )
-        },
-    }
-];
+
 type TSearch = {
     fullName: string;
     email: string;
@@ -76,6 +24,69 @@ const TableUser = () => {
         total: 0
     })
 
+    const [openViewDetail, setOpenViewDetail] = useState<boolean>(false);
+    const [dataViewDetail, setDataViewDetail] = useState<IUserTable | null>(null);
+
+    const columns: ProColumns<IUserTable>[] = [
+        {
+            dataIndex: 'index',
+            valueType: 'indexBorder',
+            width: 48,
+        },
+        {
+            title: 'Id',
+            dataIndex: '_id',
+            hideInSearch: true,
+            render(dom, entity, index, action, schema) {
+                return (
+                    <a
+
+                        onClick={() => {
+                            setDataViewDetail(entity);
+                            setOpenViewDetail(true);
+                        }}
+                        href='#'>{entity._id}</a>
+                )
+            }
+        },
+        {
+            title: 'Full Name',
+            dataIndex: 'fullName',
+        },
+        {
+            title: 'Email',
+            dataIndex: 'email',
+            copyable: true
+        },
+        {
+            title: 'Created At',
+            dataIndex: 'createdAt',
+            valueType: 'date',
+            sorter: true,
+            hideInSearch: true
+        }
+        ,
+        {
+            title: 'Created At',
+            dataIndex: 'createdAtRange',
+            valueType: 'dateRange',
+            sorter: true,
+            hideInTable: true
+        }
+        ,
+        {
+            title: 'Action',
+            hideInSearch: true,
+            render(dom, entity, index, action, schema) {
+                return (
+                    <>
+                        <EditTwoTone twoToneColor="#f57800" style={{ cursor: "pointer", marginRight: 15 }} />
+                        <DeleteTwoTone twoToneColor="#ff4d4f" style={{ cursor: 'pointer' }} />
+                    </>
+                )
+            },
+        }
+    ];
     return (
         <>
             <ProTable<IUserTable, TSearch>
@@ -148,6 +159,15 @@ const TableUser = () => {
 
                 ]}
             />
+
+            <DetailUser
+                openViewDetail={openViewDetail}
+                setOpenViewDetail={setOpenViewDetail}
+                dataViewDetail={dataViewDetail}
+                setDataViewDetail={setDataViewDetail}
+            >
+
+            </DetailUser>
         </>
     );
 };
